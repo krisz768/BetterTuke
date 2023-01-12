3caae95214182dc31b1ba50edf3eb7bb38e20d1e
krisz768 <till.krisz@gmail.com>
2023-01-12 13:05:02 +0100

    Build

--- app/src/main/java/hu/krisz768/bettertuke/ScheduleFragment/ScheduleBusTimeFragment.java
+++ app/src/main/java/hu/krisz768/bettertuke/ScheduleFragment/ScheduleBusTimeFragment.java
@@ -430,7 +430,7 @@ public class ScheduleBusTimeFragment extends Fragment {
                             }
                         });
                     }
-                    
+
                     return;
                 }
 
