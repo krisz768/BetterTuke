879c1dcd28aaa635489ee325a7705f99db1fd9fa
krisz768 <till.krisz@gmail.com>
2023-04-07 11:44:43 +0200

    Incoming date selector

--- app/src/main/java/hu/krisz768/bettertuke/IncomingBusFragment/IncomingBusStopSelectorAdapter.java
+++ app/src/main/java/hu/krisz768/bettertuke/IncomingBusFragment/IncomingBusStopSelectorAdapter.java
@@ -15,17 +15,17 @@ import hu.krisz768.bettertuke.Database.BusStops;
 import hu.krisz768.bettertuke.HelperProvider;
 import hu.krisz768.bettertuke.R;
 
-public class IncomingBusStopSelectorAdapter extends RecyclerView.Adapter<IncomingBusStopSelectorAdapter.ViewHolder>{
+public class IncomingBusStopSelectorAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private final BusStops[] BusStopList;
     private final Context ctx;
     private int SelectedStop;
     private final BottomSheetIncomingBusFragment callback;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class StopViewHolder extends RecyclerView.ViewHolder {
 
         private final Button button;
 
-        public ViewHolder(View view) {
+        public StopViewHolder(View view) {
             super(view);
             button = view.findViewById(R.id.StopButton);
         }
@@ -54,6 +54,30 @@ public class IncomingBusStopSelectorAdapter extends RecyclerView.Adapter<Incomin
         }
     }
 
+    public static class DateViewHolder extends RecyclerView.ViewHolder {
+
+        private final Button button;
+
+        public DateViewHolder(View view) {
+            super(view);
+            button = view.findViewById(R.id.SDateButton);
+        }
+
+        public void setData(BottomSheetIncomingBusFragment callback, Context ctx) {
+
+            button.setText("2023. 04. 05. 15:23");
+
+
+            //button.setOnClickListener(view -> callback.OnStopClick(Data.getId()));
+
+            TypedValue typedValue = new TypedValue();
+            ctx.getTheme().resolveAttribute(com.google.android.material.R.attr.colorSecondary, typedValue, true);
+            int color = ContextCompat.getColor(ctx, typedValue.resourceId);
+            button.setBackgroundColor(color);
+
+        }
+    }
+
     public IncomingBusStopSelectorAdapter(BusStops[] BusStopList, int SelectedStop, BottomSheetIncomingBusFragment callback, Context ctx) {
         this.BusStopList = BusStopList;
         this.SelectedStop = SelectedStop;
@@ -67,20 +91,42 @@ public class IncomingBusStopSelectorAdapter extends RecyclerView.Adapter<Incomin
 
     @NonNull
     @Override
-    public IncomingBusStopSelectorAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.incoming_bus_stop_list_recview, viewGroup, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        RecyclerView.ViewHolder result;
+        if (viewType == 0) {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.incoming_bus_stop_list_recview, viewGroup, false);
 
-        return new IncomingBusStopSelectorAdapter.ViewHolder(view);
+            result = new StopViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.incoming_bus_stop_list_datetime_recview, viewGroup, false);
+
+            result = new DateViewHolder(view);
+        }
+        return result;
     }
 
     @Override
-    public void onBindViewHolder(IncomingBusStopSelectorAdapter.ViewHolder viewHolder, final int position) {
-        viewHolder.setData(BusStopList[position],SelectedStop, callback, ctx);
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, final int position) {
+        if (viewHolder.getItemViewType() == 0) {
+            ((StopViewHolder)viewHolder).setData(BusStopList[position],SelectedStop, callback, ctx);
+        } else {
+            ((DateViewHolder)viewHolder).setData(callback, ctx);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position < BusStopList.length) {
+            return 0;
+        }
+
+        return 1;
     }
 
     @Override
     public int getItemCount() {
-        return BusStopList.length;
+        return BusStopList.length+1;
     }
 }
