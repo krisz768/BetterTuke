ae52352248fe82bad83eb386f9c38a6a3e8f2ed0
krisz768 <till.krisz@gmail.com>
2023-04-10 19:03:48 +0200

    Bug fix

--- app/src/main/java/hu/krisz768/bettertuke/IncomingBusFragment/BottomSheetIncomingBusFragment.java
+++ app/src/main/java/hu/krisz768/bettertuke/IncomingBusFragment/BottomSheetIncomingBusFragment.java
@@ -418,16 +418,17 @@ public class BottomSheetIncomingBusFragment extends Fragment {
                             Boolean IsBusStarted = serverApi.getIsBusHasStarted(incomingBusRespModel.getLineId());
                             if (IsBusStarted == null) {
                                 IsBusStarted = false;
-                            }
-                            incomingBusRespModel.setStarted(IsBusStarted);
-                            if (!IsBusStarted && ((Bj.getDepartureHour() == Integer.parseInt(Sdf.format(currentTime)) && Bj.getDepartureMinute() < Integer.parseInt(Sdf2.format(currentTime))) || Bj.getDepartureHour() < Integer.parseInt(Sdf.format(currentTime)))) {
-                                Calendar ArrTime = Calendar.getInstance();
-                                ArrTime.setTime(incomingBusRespModel.getArriveTime());
+                            } else {
+                                if (!IsBusStarted && ((Bj.getDepartureHour() == Integer.parseInt(Sdf.format(currentTime)) && Bj.getDepartureMinute() < Integer.parseInt(Sdf2.format(currentTime))) || Bj.getDepartureHour() < Integer.parseInt(Sdf.format(currentTime)))) {
+                                    Calendar ArrTime = Calendar.getInstance();
+                                    ArrTime.setTime(incomingBusRespModel.getArriveTime());
 
-                                if (ArrTime.after(Now)) {
-                                    incomingBusRespModel.setMiss(true);
+                                    if (ArrTime.after(Now)) {
+                                        incomingBusRespModel.setMiss(true);
+                                    }
                                 }
                             }
+                            incomingBusRespModel.setStarted(IsBusStarted);
                         } else {
                             incomingBusRespModel.setStarted(false);
                         }
