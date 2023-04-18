97c6c6dcd50f1c99d4b0ddb9bc2449be31c24644
krisz768 <till.krisz@gmail.com>
2023-04-18 21:15:25 +0200

    Crash fix

--- app/src/main/java/hu/krisz768/bettertuke/UserDatabase/UserDatabase.java
+++ app/src/main/java/hu/krisz768/bettertuke/UserDatabase/UserDatabase.java
@@ -86,6 +86,7 @@ public class UserDatabase {
                     .setIntent(intent)
                     .build();
 
+
             ShortcutManagerCompat.pushDynamicShortcut(Ctx, shortcut);
         }
 
