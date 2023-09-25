9433765782c810647aee0ce73b4bab977d35acef
krisz768 <till.krisz@gmail.com>
2023-09-25 09:25:43 +0200

    GTFS ContentLength check

---
+++ app/src/main/java/hu/krisz768/bettertuke/gtfs/GTFSContentLength.java
@@ -0,0 +1,28 @@
+package hu.krisz768.bettertuke.gtfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GTFSContentLength implements Runnable{
+
+    String ContentLength = "";
+    boolean Error = false;
+
+    @Override
+    public void run() {
+        try {
+            URL url = new URL( "https://mobilitas.biokom.hu/gtfs/");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(3000);
+            urlConnection.setReadTimeout(3000);
+
+            urlConnection.connect();
+            ContentLength = Integer.toString(urlConnection.getContentLength());
+        } catch (Exception e) {
+            Error = true;
+        }
+    }
+}
